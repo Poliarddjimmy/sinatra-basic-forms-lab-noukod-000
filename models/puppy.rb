22b0260a13b7644adc34a145630c8b90a0746ce9
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-06-03 13:28:59 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,9 +1,10 @@
 class Puppy
-  attr_accessor :age, :name, :breed
+  attr_accessor  :name, :breed, :age
   
-  def initialize(name, age, breed)
+  def initialize(name , age="2" , breed="black lab")
     @name = name
     @age = age
     @breed = breed
+    
   end
 end
