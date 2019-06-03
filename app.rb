95b4464c1431322d8d5c6cfe65e2c579d76ab3c6
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-06-03 13:55:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
   
   post '/' do
-    name = params[:name]
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
   
